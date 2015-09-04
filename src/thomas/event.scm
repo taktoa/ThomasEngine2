ead6e08a0fb950fa6a06d41604d77976dc6ced2a
Chap Wilkinson <cement122293@gmail.com>
2015-09-04 17:49:37 -0500

    Making event.scm compile

--- src/thomas/event.scm
+++ src/thomas/event.scm
@@ -32,9 +32,26 @@
 (define-module (thomas event)
   #:version    (0 0 1)
   #:use-module (scheme documentation)
+  #:use-module (oop goops)
+  #:use-module (ice-9 futures)
+  #:use-module (ice-9 match)
   #:export     (<evt-handler>))
 
-(define-class <evt-handler> ()
+(define undefined '())
+(define true #t)
+(define* (void #:rest args) undefined)
+(define* (mutable-set #:rest args) undefined)
+(define get-event-type void)
+(define get-x void)
+(define get-y void)
+(define get-key-code void)
+(define get-key-release-code void)
+(define set-remove! void)
+(define set-add! void)
+(define pressed-keys undefined)
+(define set-member? void)
+
+(define-class <evt-handler> (<class>)
     (pressed-keys 
       #:init-keyword #:keys
       #:init-form (mutable-set)))
@@ -46,16 +63,19 @@
    [(is-a? e <mouse-event>) (mouse-translate-event e)]
    [(is-a? e <key-event>) (key-translate-event e)]))
 
+(define-class <mouse-event> (<class>))
+(define-class <key-event> (<class>))
+
 ;; Unwrap mouse event type, x, and y, and pass them to mouse-translate
 (define (mouse-translate-event e)
-  (mouse-translate (send e get-event-type) (send e get-x) (send e get-y)))
+  (mouse-translate (get-event-type e) (get-x e) (get-y e)))
 
 ;; Translate mouse event type, x, and y to a list
 (define (mouse-translate t x y) (list t x y))
 
 ;; Unwrap the key-codes from the key-event and pass them to key-translate
 (define (key-translate-event e)
-  (key-translate (send e get-key-code) (send e get-key-release-code)))
+  (key-translate (get-key-code e) (get-key-release-code e)))
 
 ;; Translate raw key-events to more useable pairs
 (define (key-translate x y)
@@ -79,10 +99,14 @@
 ;; Utility function for determining whether or not a key is pressed
 (define-method (is-pressed? c) (set-member? pressed-keys c))
 
+;; TODO finish code
+(define* (thread-receive #:rest args) '())
+
 ;;; Class initialization
 ;; Key capture thread
-(define key-thread
-  (thread (λ ()
-           (let loop ()
-              (set-pressed-keys (translate-event (thread-receive)))
-              (loop)))))
+;(define key-thread
+;  (make-future (let loop ()
+;                 (set-pressed-keys (translate-event (thread-receive)))
+;                 (loop))))
+
+(define key-thread undefined)
